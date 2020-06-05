7397d41a5e3d52834dcde155500f647ecf6ddde6
Sofia Santos <44589228+RisingFisan@users.noreply.github.com>
2020-06-05 22:38:59 +0100

    Update Questoes1.c

--- Questoes1.c
+++ Questoes1.c
@@ -423,7 +423,7 @@ int retiraNeg (int v[], int N) {
     int i = 0;
     while(i < N) {
         if(v[i] < 0) {
-            for(int j = i; j < N; j++) v[j] = v[j + 1];
+            for(int j = i; j < N - 1; j++) v[j] = v[j + 1];
             N--;
         }
         else i++;
