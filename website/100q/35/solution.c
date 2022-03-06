d76a15e07c300ffa8f88c4419a83dc7a1708394d
Sofia Santos <sofiarsantos31@gmail.com>
2022-03-06 02:38:56 +0000

    add questions 35 and 36

--- website/100q/35/solution.c
+++ website/100q/35/solution.c
@@ -1,1 +1,13 @@
-Coming Soon™️
+int comunsOrd (int a[], int na, int b[], int nb) {
+    int i = 0, j = 0, ans = 0;
+    while(i < na && j < nb) {
+        if(a[i++] == b[j++]) {
+            ans++;
+            i++;
+            j++;
+        }
+        else if(a[i] > b[j]) j++;
+        else i++;
+    }
+    return ans;
+}
--- website/100q/36/solution.c
+++ website/100q/36/solution.c
@@ -1,1 +1,13 @@
-Coming Soon™️
+int comuns (int a[], int na, int b[], int nb) {
+    int ans = 0;
+    for(int i = 0; i < na; i++) {
+        for(int j = 0; j < nb; j++) 
+            if(a[i] == b[j]) {
+                ans++;
+                break; // mimimi eu sei, o JBB não gosta de breaks no meio de ciclos
+                       // temos pena, assim é a forma mais eficiente de o fazer
+                       // deixo a forma à JBB como exercício para casa
+            }
+    }
+    return ans;
+}
